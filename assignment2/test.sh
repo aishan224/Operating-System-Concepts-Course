7c6812b62b0e69421b77df4d0f0dfdd4a7e392eb
YunL Cai <38125783+aishan224@users.noreply.github.com>
2019-12-17 10:44:37 +0800

    Delete test.sh

--- assignment2/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-echo "Compiling"
-gcc vm.c -o vm
-echo "Running vm"
-./vm addresses.txt > out.txt    # change
-echo "Comparing with correct.txt"
-diff out.txt correct.txt
-echo "Compare Complete"
